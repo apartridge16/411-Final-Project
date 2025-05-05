0ea5c17bd50fbf7002a543b1024102c1e2863c9b
Aaron Partridge <apartridge16@gmail.com>
2025-05-04 21:19:40 -0700

    added comments

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 411-Final-Project
+
+To run the code I just used the 'make' command to generate an 'a.out' file, then I executed the a.out with './a.out'.
+
+After that the console will prompt you with which test you would like to run and you just enter the number of a test.
--- huffman.cpp
+++ huffman.cpp
@@ -7,6 +7,14 @@
 using namespace std;
 using namespace std::chrono;
 
+/******************************************************************************
+ * A simple Node struct                                                       *
+ * ch - character value                                                       *
+ * frq - frequncy of the character                                            *
+ * code - the binary code produced by the huffman tree                        *
+ * left - left child                                                          *
+ * right - right child                                                        *
+ * ****************************************************************************/
 struct Node
 {
     char ch;
@@ -18,14 +26,20 @@ struct Node
     Node(char c, int i) : ch(c), frq(i), left(nullptr), right(nullptr) {}
 };
 
+/******************************************************************************
+ * getFrequency                                                               *
+ * calculates the frequency of each character in the original message         *
+ * originalMessage - the input string to analyze                              *
+ * nodes - vector to store unique characters and their frequencies            *
+ ******************************************************************************/
 void getFrequency(string originalMessage, vector<Node *> &nodes)
 {
-
     for (char c : originalMessage)
     {
         bool found = false;
         for (Node *node : nodes)
         {
+            // if node char matches original character
             if (node->ch == c)
             {
                 node->frq++;
@@ -39,6 +53,11 @@ void getFrequency(string originalMessage, vector<Node *> &nodes)
     }
 }
 
+/******************************************************************************
+ * compareFrequencies                                                         *
+ * struct used to order nodes in the priority queue based on frequency        *
+ * returns true if the frequency of a > b                                     *
+ ******************************************************************************/
 struct compareFrequencies
 {
     bool operator()(Node *a, Node *b)
@@ -47,13 +66,22 @@ struct compareFrequencies
     }
 };
 
+/******************************************************************************
+ * makeHuffmanTree                                                            *
+ * builds a Huffman tree from a list of unique character nodes                *
+ * nodes - vector of Node pointers with initial frequencies                   *
+ * returns a pointer to the root of the constructed Huffman tree              *
+ ******************************************************************************/
 Node *makeHuffmanTree(vector<Node *> nodes)
 {
+    // initialize min-heap priority queue
     priority_queue<Node *, vector<Node *>, compareFrequencies> PQ;
 
+    // push nodes onto the queue
     for (Node *node : nodes)
         PQ.push(node);
 
+    // merge nodes until one root remains
     while (PQ.size() > 1)
     {
         Node *t1 = PQ.top();
@@ -61,6 +89,7 @@ Node *makeHuffmanTree(vector<Node *> nodes)
         Node *t2 = PQ.top();
         PQ.pop();
 
+        // create new internal node with combined frequency
         Node *tNode = new Node('\0', t1->frq + t2->frq);
 
         tNode->left = t1;
@@ -74,18 +103,35 @@ Node *makeHuffmanTree(vector<Node *> nodes)
     return root;
 }
 
+/******************************************************************************
+ * getHuffmanCodes                                                            *
+ * traverses the Huffman tree and assigns binary codes to each character      *
+ * node - current node being visited                                          *
+ * currCode - the binary string built so far                                  *
+ ******************************************************************************/
 void getHuffmanCodes(Node *node, string currCode)
 {
+    // if no node
     if (!node)
         return;
 
+    // if leaf node reached
     if (node->ch != '\0')
         node->code = currCode;
 
+    // go left and add 0
     getHuffmanCodes(node->left, currCode + "0");
+    // go right and add 1
     getHuffmanCodes(node->right, currCode + "1");
 }
 
+/******************************************************************************
+ * encode                                                                     *
+ * makes new string using the Huffman codes stored in the nodes               *
+ * originalMessage - the original input string                                *
+ * nodes - list of nodes with assigned Huffman codes                          *
+ * returns a compressed binary string                                         *
+ ******************************************************************************/
 string encode(string originalMessage, vector<Node *> nodes)
 {
     string compressedMessage;
@@ -105,6 +151,13 @@ string encode(string originalMessage, vector<Node *> nodes)
     return compressedMessage;
 }
 
+/******************************************************************************
+ * decode                                                                     *
+ * decompresses a binary string using the Huffman tree                        *
+ * compressedMessage - the binary string to decode                            *
+ * root - the root of the Huffman tree                                        *
+ * returns the original string                                                *
+ ******************************************************************************/
 string decode(string compressedMessage, Node *root)
 {
     string result;
@@ -127,15 +180,25 @@ string decode(string compressedMessage, Node *root)
     return result;
 }
 
+/******************************************************************************
+ * compareMessages                                                            *
+ * compares the original and decompressed messages for accuracy               *
+ * originalMessage - the original string before compression                   *
+ * decompressedMessage - the string after decompression                       *
+ * prints whether the decompression was successful or where a mismatch occurs *
+ ******************************************************************************/
 void compareMessages(string originalMessage, string decompressedMessage)
 {
     cout << "\nDecompressing Message\n";
+
+    // checks if length matches after decompression
     if (originalMessage.length() != decompressedMessage.length())
     {
         cout << "Differing Amounts of Data\n";
         return;
     }
 
+    // checks each character against original message
     for (int i = 0; i < originalMessage.length(); i++)
     {
         if (originalMessage[i] != decompressedMessage[i])
@@ -154,10 +217,14 @@ int main()
     char choice = 'y';
     do
     {
+        // Test cases came from https://corpus.canterbury.ac.nz/descriptions/
         string TEST_CASES[7] = {"artificl/aaa.txt", "artificl/alphabet.txt", "artificl/random.txt", "large/bible.txt", "large/E.coli", "large/pi.txt", "large/world192.txt"};
+
+        // If you want to add more test cases, add the local path to this array and increment the size of the array
+        // Then enter the corresponding number when prompted
+
         int testChoice = 0;
 
-        // Test cases came from https://corpus.canterbury.ac.nz/descriptions/
         cout << "\n\n0) The letter 'a', repeated 100,000 times." << endl;
         cout << "1) Enough repetitions of the alphabet to fill 100,000 characters" << endl;
         cout << "2) 100,000 characters, randomly selected from [a-z|A-Z|0-9|!| ] (alphabet size 64)" << endl;
